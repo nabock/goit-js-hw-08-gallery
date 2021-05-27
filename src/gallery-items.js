df23dc98a4ed8ac9355374161fd7f0dfb3b277b0
Nabok Serhii <s.nabock@gmail.com>
2021-05-27 04:27:17 +0300

    update js

--- src/gallery-items.js
+++ src/gallery-items.js
@@ -1,4 +1,6 @@
-export default [
+// export default
+
+const images = [
   {
     preview:
       'https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825__340.jpg',
@@ -63,3 +65,148 @@ export default [
     description: 'Lighthouse Coast Sea',
   },
 ];
+
+// console.log(images);
+
+
+
+const gallaryContainer = document.querySelector(".js-gallery");
+
+const modal = document.querySelector(".js-lightbox");
+
+const modalClose = document.querySelector('[data-action="close-lightbox"]');
+
+const modalOriginalImage = document.querySelector(".lightbox__image")
+
+
+// Added murkup gallery
+
+
+const imgMarkup = createImageCardsMarkup(images)
+
+gallaryContainer.insertAdjacentHTML('beforeend', imgMarkup)
+
+
+
+function createImageCardsMarkup(images) {
+  return images
+    .map(({ preview, original, description }) => {
+
+      return `
+         <li class="gallery__item">
+            <a
+                class="gallery__link"
+             href="${original}"
+            >
+                <img
+                    class="gallery__image"
+                    src="${preview}"
+                    data-source="${original}"
+                    alt="${description}"
+                />
+            </a>
+         </li >
+            `;
+
+    })
+    .join('');
+}
+
+
+
+
+
+// Opened modal
+
+gallaryContainer.addEventListener('click', onGallaryContainerClick)
+
+function onGallaryContainerClick(event) {
+
+  event.preventDefault()
+
+  const isImageSwatchEl = event.target.classList.contains("gallery__image")
+
+  if (!isImageSwatchEl) {
+    return
+  }
+
+  const imageUrl = event.target.dataset.source;
+
+  modal.classList.add("is-open");
+
+  modalOriginalImage.src = imageUrl
+
+  // console.log(imageUrl);
+}
+
+
+
+// Closed modal 
+
+modalClose.addEventListener('click', onModalCloseClick);
+
+function onModalCloseClick(event) {
+
+  modal.classList.remove("is-open");
+
+  modalOriginalImage.src = ''
+
+  // console.log("close", modalOriginalImage.src = '');
+}
+
+
+
+
+// // Закрытие модалки  клавишей esc ---------------------
+
+// document.addEventListener('keydown', onEscClick);
+// function onEscClick(event) {
+//   if (event.keyCode !== 27) {
+//     return
+//   }
+//   modal.classList.remove("is-open");
+//   modalImage.src = ''
+// }
+// //-----------------------------------------------------------
+
+// // Закрытие модалки кликом по полю модалки ---------------------
+
+// modal.addEventListener('click', onModalWindowClick);
+
+// function onModalWindowClick(event) {
+//   const isModalImageEl = event.target.classList.contains("lightbox__image");
+//   if (isModalImageEl) {
+//     return
+//   }
+//   modal.classList.remove("is-open");
+//   modalImage.src = ''
+// }
+// //---------------------------------------------------------------
+// // Слайдшоу стрелками  ArrowLeft || ArrowRight---------------------
+
+// const arrayImages = [];
+// images.forEach(({ original }) => {
+//   arrayImages.push(original);
+
+// })
+// // console.log(arrayImages);
+
+// document.addEventListener('keydown', onArrowClick);
+// function onArrowClick(event) {
+//   let newIndex;
+//   const currentId = arrayImages.indexOf(modalImage.src);
+//   if (event.key === 'ArrowLeft') {
+//     if (currentId > -1) {
+//       newIndex = currentId - 1;
+//     }
+//     if (newIndex === -1) {
+//       newIndex = arrayImages.length - 1;
+//     }
+//   } else if (event.key === 'ArrowRight') {
+//     newIndex = currentId + 1;
+//     if (newIndex === arrayImages.length) {
+//       newIndex = 0;
+//     }
+//   }
+//   modalImage.src = arrayImages[newIndex];
+// };
